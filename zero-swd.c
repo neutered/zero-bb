c24ad709292d8645c24488dda7dbb3799e44b60f
dpc <weasel@cs.stanford.edu>
2018-08-04 15:44:45 -0700

    test for read

--- zero-swd.c
+++ zero-swd.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "libpin.h"
 
-static void resync(struct pinctl* pins)
+static void resync(struct pinctl* pins, int idle)
 {
   static const uint8_t bs[] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00 };
-  pins_write(pins, bs, sizeof(bs) * 8);
+  pins_write(pins, bs, (sizeof(bs) - !idle) * 8);
 }
 
 static void jtag2swd(struct pinctl* pins)
@@ -17,6 +18,43 @@ static void jtag2swd(struct pinctl* pins)
   pins_write(pins, bs, sizeof(bs) * 8);
 }
 
+/* on the wire format
+ *  1 - start
+ *  x - ap == 1, dp == 0
+ *  y - write == 0, read == 1
+ *  r - register[2]
+ *  r - register[3]
+ *  p - parity
+ *  0 - stop
+ *  1 - park
+ */
+static uint8_t opcode(int ap, int reg, int rw)
+{
+  uint8_t rv = 0x81;
+  if (ap) rv |= 0x02;
+  if (rw) rv |= 0x04;
+  rv |= (reg & 0x0f) << 1;
+  if (__builtin_popcount(rv) & 0x01) rv |= 0x20;
+  return rv;
+}
+
+/* returns status
+ *  [0] - ok
+ *  [1] - xxx
+ *  [2] - yyy
+ */
+static uint8_t send_op(struct pinctl* pins, int ap, int reg, int rw)
+{
+  uint8_t op = opcode(ap, reg, rw);
+fprintf(stderr, "%s:%d: op:%02x\n", __func__, __LINE__, op);
+  pins_write(pins, &op, 8);
+  
+  uint8_t status;
+  pins_read(pins, &status, 3);
+
+  return status;
+}
+
 int main(int argc, char** argv)
 {
   int rv = EXIT_FAILURE;
@@ -24,11 +62,13 @@ int main(int argc, char** argv)
   if (!pins) goto err_exit;
 
   /* reset-y bits for the debug port */
-  resync(pins);
+  resync(pins, 0);
   jtag2swd(pins);
-  resync(pins);
+  resync(pins, 1);
 
   /* reading id takes the debug port out of reset */
+  uint8_t status = send_op(pins, 0, 0, 1);
+  fprintf(stderr, "%s:%d: status:%02x\n", __func__, __LINE__, status);
   
   pins_close(pins);
   rv = EXIT_SUCCESS;
